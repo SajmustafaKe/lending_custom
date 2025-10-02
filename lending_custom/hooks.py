42a78d3d0785005ec14f7e98ebbb04d457f17bf0
Sajmustafake <sajmustafa@hotmail.com>
2025-10-02 13:01:13 +0300

    Hooks Patch

--- lending_custom/hooks.py
+++ lending_custom/hooks.py
@@ -85,6 +85,13 @@ app_license = "mit"
 # before_install = "lending_custom.install.before_install"
 # after_install = "lending_custom.install.after_install"
 
+# Patches
+# -------
+
+patches = [
+	"lending_custom.patches.add_interest_calculation_method"
+]
+
 # Uninstallation
 # ------------
 
